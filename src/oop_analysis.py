131670aba89e11f71542c80eac5dafc47205f055
ep413 <zahs@uni-heidelberg.de>
2022-03-03 11:17:52 +0100

    added oop analysis script

---
+++ src/oop_analysis.py
@@ -0,0 +1,79 @@
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+import time
+import sys
+
+# sns.set_theme(style="darkgrid")
+
+
+##############################################################
+#                    helper functions                        #
+##############################################################
+
+
+def df_to_np_array():
+    pass
+
+
+def create_plot(x_col, y_col, file_out):
+    # plot and save as pdf
+    pass
+
+
+##############################################################
+#                    main function                           #
+##############################################################
+def main(argv):
+    pass
+
+
+##############################################################
+#                    main classes                            #
+##############################################################
+
+
+class in_out:
+    def __init__(self, df_name: str):
+        self.df_name = df_name
+
+    """
+    :param str : df_name : name of df
+    """
+    # import data into df
+    def read_csv(self, file_in):
+        return self.df_name
+
+    def write_to_csv(self, file_out):
+        pass
+
+
+class analysis:
+    def __init__(self, df: str):
+        self.df = df
+        self.df_filtered = None
+
+    def filter_columns_by_variance(self, treshold):
+        return self.df_filtered
+
+    def create_corr_matrix(self):
+        return  # sorted r2 values as pandas sequence
+
+    def calc_euclidean_dist(self):
+        return
+
+    def fourier_trafo(self):
+        pass
+
+
+##############################################################
+#                                                            #
+##############################################################
+
+if __name__ == "__main__":
+    start = time.time()
+    pass
+    end = time.time()
+    print("Runtime: %.3fs" % (end - start))
+    sys.exit()
